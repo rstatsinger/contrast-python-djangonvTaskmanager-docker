58b48d76722203070540ead49252203505597b64
Robert Statsinger <robert.statsinger@contrastsecurity.com>
2021-07-30 09:04:01 -0700

    Update run_app_docker.sh

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# python manage.py runserver 0.0.0.0:8000
+
 export CONTRAST_CONFIG_PATH=/app/contrast_security.yaml
+export CONTRAST__AGENT__LOGGER__STDOUT=true
+export CONTRAST__AGENT__SERVICE_LOGGER__STDOUT=true
+export CONTRAST__APPLICATION__NAME=djangonvTaskManager
+export CONTRAST__ASSESS__ENABLE=true
+export CONTRAST__PROTECT__ENABLE=false
+
 python3 manage.py runserver 0.0.0.0:8000
